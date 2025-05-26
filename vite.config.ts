c8eb33fdd60cdf3426b5ef851b53ea534d4eef19
Andrew Dutka <andrewadutka@gmail.com>
2025-05-26 01:39:20 -0300

    vercel plugins and dependencies added

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+    server: {
+        port: process.env.PORT as unknown as number,
+    },
+    plugins: [react(), tailwindcss(), vercel()],
 });
