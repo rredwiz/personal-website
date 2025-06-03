a4d33aebfd49da877bdb433e320b93a53710eb84
Andrew <andrewadutka@gmail.com>
2025-06-03 18:07:10 -0300

    enabled dark theme and set body and html colors to background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-            <body>
+        <html lang="en" className="bg-[#232323]">
+            <body className="bg-[#232323]">
                 {children}
                 <Analytics />
             </body>
