f9b113ccc4a40b266d6a7e3ef840108e7839259d
Andrew Dutka <andrewadutka@gmail.com>
2025-06-08 21:50:34 -0300

    what is this monstrosity of a learning experence of how to use a backend

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,30 +1,25 @@
 from flask import Flask, jsonify, request
 import requests
-
 from dotenv import load_dotenv
+
 load_dotenv()
 from apis import spotify, github
 
 app = Flask(__name__)
 
-# test route
-@app.route('/')
-def test():
-    data = spotify.test()
-    if not data:
-        return jsonify({"error":"bad thing occurred"}), 500
-    return jsonify(data), 200
 
-@app.route('/github')
+@app.route("/github")
 def get_github_recent_commits():
-    return 'will make soon'
+    return "will make soon"
 
-@app.route('/spotify')
+
+@app.route("/spotify")
 def get_spotify_info():
     data = spotify.main_request()
     if not data:
         return jsonify({"error": "the request was unsuccessful"}), 500
     return jsonify(data), 200
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
