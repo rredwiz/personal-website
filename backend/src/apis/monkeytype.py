181235367f71642a62772902c8a5937d20c6aff7
Andrew <andrewadutka@gmail.com>
2025-06-12 13:15:44 -0300

    added monkeytype endpoint for typing scores to backend

---
+++ backend/src/apis/monkeytype.py
@@ -0,0 +1,66 @@
+import os
+import requests
+
+APE_KEY = os.environ.get("APE_KEY")
+
+avg_wpm_url = "https://api.monkeytype.com/results"
+best_wpm_url = "https://api.monkeytype.com/users/personalBests"
+header = {"Authorization": f"ApeKey {APE_KEY}"}
+avg_wpm_params = {"limit": "100"}
+best_wpm_params = {"mode": "time"}
+
+
+def get_monkeytype_scores_info():
+    avg_wpm = get_recent_100_scores_avg()
+    best_wpm = get_best_score()
+    return {"averagewpm": avg_wpm, "bestwpm": best_wpm}
+
+
+def get_best_score():
+    response = requests.get(best_wpm_url, headers=header, params=best_wpm_params)
+    if response.status_code == 200:
+        data = response.json()
+        print("best score was:", filter_best_score(data))
+        return filter_best_score(data)
+    print(
+        "something went wrong fetching best score",
+        response.status_code,
+        response.content,
+    )
+
+
+def get_recent_100_scores_avg():
+    response = requests.get(avg_wpm_url, headers=header, params=avg_wpm_params)
+    if response.status_code == 200:
+        data = response.json()
+        print("average wpm was:", filter_avg_scores(data))
+        return filter_avg_scores(data)
+    print("failed to fetch", response.status_code, response.content)
+
+
+def filter_avg_scores(data):
+    wpm = 0
+    count = 0
+    for test in data.get("data"):
+        if (
+            test.get("language") == "english_1k"
+            or test.get("language") == "english"
+            and test.get("mode") == "time"
+            and test.get("mode2") == 15
+        ):
+            wpm += test.get("wpm")
+            count += 1
+    if count == 0:
+        print("count was 0 in filter_scores, therefore averagewpm is 0.0")
+        return 0.0
+    avg_wpm = wpm / count
+    return avg_wpm
+
+
+def filter_best_score(data):
+    all_best_scores_15_seconds = data.get("data").get("15")
+    for mode in all_best_scores_15_seconds:
+        if mode.get("language") == "english":
+            best_score_english = mode.get("wpm")
+            return best_score_english
+    print("something went wrong filtering")
--- backend/src/app.py
+++ backend/src/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 
 load_dotenv()
-from src.apis import lastfm, github
+from src.apis import lastfm, github, monkeytype
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +12,16 @@ CORS(app)
 @app.route("/")
 def render_health_check():
     return "HEALTH CHECK 200"
+
+
+@app.route("/monkeytype")
+def get_typing_stats():
+    data = monkeytype.get_monkeytype_scores_info()
+    if not data:
+        return jsonify(
+            {"error": "fask receieved None from monkeytype.get_recent_typing_scores"}
+        )
+    return jsonify(data), 200
 
 
 @app.route("/github")
@@ -24,7 +34,7 @@ def get_lastfm_scrobble():
     data = lastfm.get_recent()
     if not data:
         return (
-            jsonify({"error": "flask received None from lastfm.get_recent()"}),
+            jsonify({"error": "flask received None from lastfm.get_recent"}),
             500,
         )
     return jsonify(data), 200
